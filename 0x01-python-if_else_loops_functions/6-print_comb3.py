5b1bf3f95d697b85f595caed71d6221b25422552
dacastanogo <dcastanogomez@gmail.com>
2019-09-12 23:23:03 -0500

    correcting error in 6 task

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
-  
 #!/usr/bin/python3
-for first in range(0, 10):
-    for second in range(0, 10):
-        if first == 8 and second == 9:
-            print("{}{}".format(first, second))
-        elif second > first:
-            print("{}{}".format(first, second), end=', ')
+for i in range(0, 9):
+    for j in range(i + 1, 10):
+        print("{:d}".format(i), end='')
+        if i == 8 and j == 9:
+            print("{:d}".format(j), end='\n')
+        else:
+            print("{:d}".format(j), end=", ")
